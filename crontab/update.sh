a9ba41765611adcee5184f798166525ccaa2f15b
Nelson Chen <crazysim@gmail.com>
2013-10-26 00:59:32 -0700

    timeout for aria2c

--- crontab/update.sh
+++ crontab/update.sh
@@ -3,6 +3,6 @@ cd ~/extreme-board
 git pull
 #rake download_json
 rm -rf tmp/aria
-aria2c -Z -P -d tmp/aria "https://ieee.hackerrank.com/rest/contests/ieeextreme7/leaderboard?offset=[0-2000:10]&_=1382769745980"
+aria2c -t 10 -Z -P -d tmp/aria "https://ieee.hackerrank.com/rest/contests/ieeextreme7/leaderboard?offset=[0-2000:10]&_=1382769745980" 
 touch content/index.haml
 nanoc co
