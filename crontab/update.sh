d6c237d2a15bbb2b7711ef1f71c7dfaf9d542107
Nelson Chen <crazysim@gmail.com>
2013-10-26 00:19:52 -0700

    delelte right folder

--- crontab/update.sh
+++ crontab/update.sh
@@ -2,7 +2,7 @@
 cd ~/extreme-board
 git pull
 #rake download_json
-rm -rf tmp/aria2
+rm -rf tmp/aria
 aria2c -Z -P -d tmp/aria "https://ieee.hackerrank.com/rest/contests/ieeextreme7/leaderboard?offset=[0-2000:10]&_=1382769745980"
 touch content/index.haml
 nanoc co
